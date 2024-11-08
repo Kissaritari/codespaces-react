0b9f52aa5727cf7d2c1bbbf95398a41262ae6ade
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2024-11-08 09:07:02 +0000

    emeag

--- src/components/CatCard.tsx
+++ src/components/CatCard.tsx
@@ -14,7 +14,7 @@ const handleOnClick = ()=> {
 }
     return (
         <div className="flex items-center flex-col mx-20">
-            <img className="rounded-lg mb-2 max-h-96" src={catKuva}></img>
+            <img className="rounded-lg mb-2 max-h-dvh" src={catKuva}></img>
             <button onClick={handleOnClick} className="max-w-20 px-3 py-1 bg-slate-500 rounded-md hover:bg-slate-400 duration-100n">kissa</button>
         </div>
     )
